34907660217a21016ed3e9fd3471d889cd27972a
Tomáš Izák <tomik.izak@gmail.com>
2010-10-25 07:46:57 +0000

    git-svn-id: svn+ssh://merlin.fit.vutbr.cz/svn/PDB03/trunk@14 bda6da93-004a-4ae9-8e07-715c10848801

--- src/cz/vutbr/fit/pdb03/DataBase.java
+++ src/cz/vutbr/fit/pdb03/DataBase.java
@@ -10,8 +10,8 @@ import oracle.jdbc.OracleResultSet;
 import oracle.jdbc.OraclePreparedStatement;
 
 /**
- * Knihovna pro pr√°ci s datab√°z√≠
- * @author xizakt00 Tom√°≈° I≈æ√°k
+ * Knihovna pro pr·ci s datab·zÌ
+ * @author xizakt00 Tom·ö Iû·k
  * 
  */
 public class DataBase {
@@ -23,6 +23,15 @@ public class DataBase {
      * private string to store url of database connection
      */
     private static String connectionString="@berta.fit.vutbr.cz:1521:stud";
+    /**
+     * 
+     * @return true if connection is alive, 0 if not connected
+     */
+    public boolean isConnected()
+    {
+        if (con==null) return false;
+        else return true;
+    }
     /**
      * Function for connectin to Oracle database
      * @param username username for database connection
@@ -42,7 +51,7 @@ connectionString);
             throw new Exception("SQLException: " + ex.getMessage());
 	}
 
-        if(con== null){
+        if(! isConnected()){
             throw new Exception("Could not connect");
 	}
     }
