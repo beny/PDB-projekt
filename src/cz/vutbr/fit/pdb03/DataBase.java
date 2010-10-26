7d0655d49130bfc5967e225b1e3ef2c8121fe724
Tomáš Izák <tomik.izak@gmail.com>
2010-10-26 13:04:09 +0000

    dalsi update
    git-svn-id: svn+ssh://merlin.fit.vutbr.cz/svn/PDB03/trunk@16 bda6da93-004a-4ae9-8e07-715c10848801

--- src/cz/vutbr/fit/pdb03/DataBase.java
+++ src/cz/vutbr/fit/pdb03/DataBase.java
@@ -1,13 +1,16 @@
 package cz.vutbr.fit.pdb03;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.DriverManager;
 import java.sql.Statement;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+//import ojdbc6.jar from oraclelib.zip/oraclelib/jdbc located in WIS
 import oracle.jdbc.OracleResultSet;
 import oracle.jdbc.OraclePreparedStatement;
+//import ordim.jar from oraclelib.zip/oraclelib/ord located in WIS
+import oracle.ord.im.OrdImage;
+import oracle.ord.im.OrdImageSignature;
 
 /**
  * Knihovna pro práci s databází
@@ -69,19 +72,26 @@ connectionString);
         }
     }
     /**
-     * Selects a random thumbnail of an animal, it's used for an illustrating found results
+     * Selects a random thumbnail or all photos of an animal, it's used for an illustrating found results
      *
      * TO DO: return gained data
      *
      * @param id ID of an animal
+     * @param all true if return all pictures of an animal, false if return only one thumbnail
      * @throws SQLException
      */
-    private void selectThumbnail(int id) throws SQLException{
+    private void selectPicture(int id, boolean all) throws SQLException{
         Statement stat=con.createStatement();
-        String SQLquery = "SELECT TOP 1 animal_photo FROM animal_photo WHERE animal_id="+Integer.toString(id);
+        String SQLquery;
+        if (all){
+            SQLquery = "SELECT animal_photo FROM animal_photo WHERE animal_id="+Integer.toString(id);
+        } else {
+            SQLquery = "SELECT TOP 1 animal_photo FROM animal_photo WHERE animal_id="+Integer.toString(id);
+        }
         OracleResultSet rset=(OracleResultSet)stat.executeQuery(SQLquery);
         rset.next();
         rset.getInt("animal_id");
+        stat.close();
         return;
     }
     /**
@@ -100,6 +110,7 @@ connectionString);
         OracleResultSet rset=null;
         if (genus == null ? "" == null : genus.equals("")) {
             if (family == null ? "" == null : family.equals("")) {
+                stat.close();
                 return;
             } else {
                 rset = (OracleResultSet)stat.executeQuery(SQLquery+"family='"+family+"' or family_lat='"+family+"'");
@@ -112,8 +123,49 @@ connectionString);
             }
         }
         while (rset.next()){
-            selectThumbnail(rset.getInt("animal_id"));
+            selectPicture(rset.getInt("animal_id"),false);
         }
+        stat.close();
         return;
+    }
+    /**
+     * Uploads image into choosen table
+     *
+     * TO DO: implement parameter for choosen table
+     *
+     * @param animal_id ID of an animal
+     * @param choosen_table
+     * @param filename Filename of a picture for upload
+     * @throws SQLException
+     * @throws IOException
+     */
+    public void uploadImage(int animal_id, int choosen_table, String filename) throws SQLException, IOException{
+        con.setAutoCommit(false);
+        Statement stat=con.createStatement();
+        String SQLquery = ("SELECT animal_photo_seq.nextval FROM dual");
+        OracleResultSet rset = (OracleResultSet)stat.executeQuery(SQLquery);
+        rset.next();
+        int nextval = rset.getInt("nextval");
+        SQLquery = "INSERT INTO animal_photo (animal_id, photo_id, photo, photo_sig) VALUES ("+nextval+", " + animal_id + ", ordsys.ordimage.init(), ordsys.ordimagesignature.init())";
+        stat.execute(SQLquery);
+        SQLquery = "SELECT image, signature FROM animal_photo WHERE animal_id = "+ nextval + " FOR UPDATE";
+        rset = (OracleResultSet)stat.executeQuery(SQLquery);
+        rset.next();
+        OrdImage imageProxy =(OrdImage)rset.getORAData("photo", OrdImage.getORADataFactory());
+        OrdImageSignature signatureProxy =(OrdImageSignature)rset.getCustomDatum("photo_sig", OrdImageSignature.getFactory());
+        rset.close();
+        imageProxy.loadDataFromFile(filename);
+        imageProxy.setProperties();
+        signatureProxy.generateSignature(imageProxy);
+        SQLquery = "UPDATE animal_photo SET photo=?, photo_sig=? where animal_id=?";
+        OraclePreparedStatement opstmt =(OraclePreparedStatement)con.prepareStatement(SQLquery);
+        opstmt.setCustomDatum(1, imageProxy);
+        opstmt.setCustomDatum(2, signatureProxy);
+        opstmt.setInt(3, nextval);
+        opstmt.execute();
+        opstmt.close();
+
+        con.commit();
+        con.setAutoCommit(true);
     }
 }
