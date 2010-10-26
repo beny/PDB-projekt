163b9a3a8a5a934e4bceedff8cf76a3b7cb4d64e
Tomáš Izák <tomik.izak@gmail.com>
2010-10-26 09:32:19 +0000

    netestovano, pridane funkce nejsou kompletní, čekám, až jak se dohodnem na rozhraní
    git-svn-id: svn+ssh://merlin.fit.vutbr.cz/svn/PDB03/trunk@15 bda6da93-004a-4ae9-8e07-715c10848801

--- src/cz/vutbr/fit/pdb03/DataBase.java
+++ src/cz/vutbr/fit/pdb03/DataBase.java
@@ -10,9 +10,8 @@ import oracle.jdbc.OracleResultSet;
 import oracle.jdbc.OraclePreparedStatement;
 
 /**
- * Knihovna pro pr·ci s datab·zÌ
- * @author xizakt00 Tom·ö Iû·k
- * 
+ * Knihovna pro pr√°ci s datab√°z√≠
+ * @author xizakt00 Tom√°≈° I≈æ√°k
  */
 public class DataBase {
     /**
@@ -54,6 +53,7 @@ connectionString);
         if(! isConnected()){
             throw new Exception("Could not connect");
 	}
+        con.setAutoCommit(true);
     }
     /**
      * Function for disconnecting from database
@@ -67,5 +67,53 @@ connectionString);
             }
             //System.out.println("Disconnected");
         }
+    }
+    /**
+     * Selects a random thumbnail of an animal, it's used for an illustrating found results
+     *
+     * TO DO: return gained data
+     *
+     * @param id ID of an animal
+     * @throws SQLException
+     */
+    private void selectThumbnail(int id) throws SQLException{
+        Statement stat=con.createStatement();
+        String SQLquery = "SELECT TOP 1 animal_photo FROM animal_photo WHERE animal_id="+Integer.toString(id);
+        OracleResultSet rset=(OracleResultSet)stat.executeQuery(SQLquery);
+        rset.next();
+        rset.getInt("animal_id");
+        return;
+    }
+    /**
+     * Function for searching animals by their names - choose one notation - latin or other language - do not blend!
+     * No need to fill both params, replace blank param by null or "".
+     *
+     * TO DO: return gained data
+     *
+     * @param genus genus name of an animal
+     * @param family family name of an animal
+     * @throws SQLException
+     */
+    public void searchAnimals(String genus,String family) throws SQLException{
+        Statement stat=con.createStatement();
+        String SQLquery = "SELECT animal_id,genus,family,genus_lat,family_lat FROM animals WHERE ";
+        OracleResultSet rset=null;
+        if (genus == null ? "" == null : genus.equals("")) {
+            if (family == null ? "" == null : family.equals("")) {
+                return;
+            } else {
+                rset = (OracleResultSet)stat.executeQuery(SQLquery+"family='"+family+"' or family_lat='"+family+"'");
+            }
+        } else {
+            if (family == null ? "" == null : family.equals("")) {
+                rset = (OracleResultSet)stat.executeQuery(SQLquery+"genus='"+genus+"' or genus_lat='"+genus+"'");
+            } else {
+                rset = (OracleResultSet)stat.executeQuery(SQLquery+"(family='"+family+"' and genus='"+genus+"') or (family_lat='"+family+"' and genus_lat='"+genus+"')");
+            }
+        }
+        while (rset.next()){
+            selectThumbnail(rset.getInt("animal_id"));
+        }
+        return;
     }
 }
